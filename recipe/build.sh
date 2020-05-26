88faa24389b2df18ad28f12f49128b64b54a5190
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-26 08:34:10 -0400

    strace aarcch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,12 @@ set -x
 #    sed -i '/udp_no_autobind/d' ./test/test-list.h
 # fi
 
-if [ "${target_platform}" == "linux-aarch64" ]; then
+# if [ "${target_platform}" == "linux-aarch64" ]; then
    # These failures have been reported upstream
    # https://github.com/libuv/libuv/issues/2867
-   sed -i '/random_async/d' ./test/test-list.h
-   sed -i '/random_sync/d' ./test/test-list.h
-fi
+#    sed -i '/random_async/d' ./test/test-list.h
+#    sed -i '/random_sync/d' ./test/test-list.h
+# fi
 
 # This particular test fails on osx
 if [ "${target_platform}" == 'osx-64' ]; then
@@ -32,5 +32,6 @@ LIBTOOLIZE=libtoolize sh ./autogen.sh
    --prefix="$PREFIX" \
 
 make
+strace -s 256 -f build/uv_run_tests_a random_sync random_sync
 make check
 make install
